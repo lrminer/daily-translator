ae8b42ca9a5729aedbfa3b2b41af3e249f83e3b7
lrminer <60660824+lrmgraytv@users.noreply.github.com>
2020-05-04 10:16:45 -0400

    react-swipeables added

--- app/src/Translatable.jsx
+++ app/src/Translatable.jsx
@@ -1,15 +1,62 @@
 import React, { useEffect, useState } from "react";
+import { useSwipeable } from "react-swipeable";
 
+const initialState = { isSpanish: false, sliding: false, dir: "ENGLISH" };
 const Translatable = (props) => {
-  const [isSpanish, setIsSpanish] = useState(false);
+  // const [isSpanish, setIsSpanish] = useState(initialState.isSpanish);
 
-  useEffect(() => {}, []);
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
+  const slide = (dir) => {
+    dispatch({ type: dir, isSpanish: "SPANISH" });
+    setTimeout(() => {
+      dispatch({ type: "stopSliding" });
+    }, 50);
+  };
+
+  const handlers = useSwipeable({
+    onSwipedLeft: () => slide("ENGLISH"),
+    onSwipedRight: () => slide("SPANISH"),
+    preventDefaultTouchmoveEvent: true,
+    trackMouse: true,
+  });
+
+  // useEffect(() => {}, []);
 
   return (
-    <div style={{ marginTop: 10 }} onClick={() => setIsSpanish(!isSpanish)}>
-      {(isSpanish && props.spanish) || props.english}
+    <div
+      {...handlers}
+      style={{ marginTop: 10 }}
+      // onClick={() => setIsSpanish(!isSpanish)}
+    >
+      {(state.isSpanish && props.spanish) || props.english}
     </div>
   );
 };
+
+function reducer(state, { type, language }) {
+  switch (type) {
+    case "reset":
+      return initialState;
+    case "ENGLISH":
+      return {
+        ...state,
+        dir: "ENGLISH",
+        sliding: true,
+        isSpanish: false,
+      };
+    case "SPANISH":
+      return {
+        ...state,
+        dir: "SPANISH",
+        sliding: true,
+        isSpanish: true,
+      };
+    case "stopSliding":
+      return { ...state, sliding: false };
+    default:
+      return state;
+  }
+}
 
 export default Translatable;
